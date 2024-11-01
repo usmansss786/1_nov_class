abeeae95d8bb06e330eadad954cf9a5a484562a6
usmansss786 <usmanshamas4515@gmail.com>
2024-11-01 23:59:52 +0500

    practice python

---
+++ main.py
@@ -0,0 +1,15 @@
+#print("Usman","ali","Umer", sep=" _ ")
+
+#print("Muhammad \"Usman\" \'Shamas\'")
+
+
+Name = "Usman"
+print(type(Name))
+
+#someone said 
+#                 "Ali" is a 'good' boy
+#        but  
+#he often makes mistakes 
+age= int(input("write your age:"))
+print("the age is" , age)
+print(type(age))
